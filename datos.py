3509910a8e395c3deee53f3feaf12398874bb4af
cuellarbarrionuevo <juanjosecuellarherrera@gmail.com>
2025-03-22 10:24:02 +0100

    Eliminar archivo del seguimiento de Git

--- datos.py
+++
@@ -1,8 +0,0 @@
-#Direcciones.
-train_dir ='D:/Python_papa/chest_xray/train'
-test_dir = 'D:/Python_papa/chest_xray/test'
-val_dir = 'D:/Python_papa/chest_xray/val'
-# Parametros
-epochs_v=30
-tamano_poblacion_v = 10
-generaciones_v = 5
